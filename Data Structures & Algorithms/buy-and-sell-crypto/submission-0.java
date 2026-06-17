2d78dac979e26428ad6403fbb1a8b247b8353e2b
Gnani104 <gnani.prasanth@gmail.com>
2026-06-17 15:02:36 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, maxProf = 0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i;j<prices.length;j++){
+                profit = prices[j] - prices[i];
+                maxProf = Math.max(profit, maxProf);
+            }
+        }
+        return maxProf;
+    }
+}
